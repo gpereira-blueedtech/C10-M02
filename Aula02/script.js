e5a2eb35ad2bf897190c9c9638031a543e2d2e45
Gabriel Lima <gpereira@blueedtech.com.br>
2021-10-27 23:03:08 -0300

    Aula 03 - Flex box e JS

---
+++ Aula02/script.js
@@ -0,0 +1,33 @@
+const btnTexto = document.querySelector("#btn-texto"); // Captura o botão no DOM pelo id
+const secaoPrimeira = document.querySelector("#primeira");
+const titulo = document.querySelector("#titulo");
+
+btnTexto.addEventListener("click", function () { // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
+    const tagH1 = document.createElement("h1"); // Cria uma tag h1 e adiciona a let tagH1
+    const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
+    
+    tagH1.appendChild(h1Text); // Adiciona a tag H1 o texto criado acima
+    secaoPrimeira.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
+});
+
+// <h1> </h1>
+
+// "Texto de exemplo"
+
+// <h1>"Texto de exemplo"</h1>
+
+// <body>
+//    ...
+//      <h1>"Texto de exemplo"</h1>    
+// </body>
+
+titulo.addEventListener("mouseover", function() {
+    titulo.style.color = "green";
+});
+
+titulo.addEventListener("mouseout", function() {
+    titulo.style.color = "black";
+});
+
+const frases = ["Frase 1", "Frase 2", "Frase 3"]
+frases[]
