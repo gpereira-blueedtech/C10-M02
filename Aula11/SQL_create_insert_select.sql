0c5960ae3fe409b16e3a76a762af29627099015a
Gabriel Lima <gpereira@blueedtech.com.br>
2021-11-09 19:05:01 -0300

    Alter table, update e delete

--- Aula11/SQL_create_insert_select.sql
+++ Aula11/SQL_create_insert_select.sql
@@ -6,7 +6,7 @@ CREATE TABLE pessoas (
 );
 
 INSERT INTO pessoas (nome, sobrenome, idade) VALUES 
-('Marcos','Rodrigues',52),
+('Bruno','Dutra',20),
 ('Joyce', 'Prado', 20),
 ('Daniela','Farias',18),
 ('Marcos','Domingues',30);
@@ -23,3 +23,25 @@ SELECT * FROM pessoas WHERE nome ILIKE 'B%'; -- Começados em B
 
 SELECT * FROM pessoas WHERE nome ILIKE '%o%' OR idade <= 20; -- Duas (ou mais) condições, em que PELO MENOS UMA deve ser verdadeira
 SELECT * FROM pessoas WHERE nome ILIKE '%o%' AND idade <= 20; -- Duas (ou mais) condições, em que TODAS ELAS devem ser verdadeiras
+
+
+
+
+ALTER TABLE pessoas 
+ADD COLUMN cidade VARCHAR;
+
+SELECT * FROM pessoas ORDER by id;
+
+UPDATE pessoas
+SET idade = 16 WHERE id = 1
+
+UPDATE pessoas
+SET idade = 18 WHERE id = 2 OR id = 5;
+
+UPDATE pessoas
+SET cidade = 'Londrina';
+
+
+DELETE FROM pessoas
+WHERE id = 11
+
